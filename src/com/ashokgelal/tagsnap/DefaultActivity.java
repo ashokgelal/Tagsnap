3423e4c99ff9571571598052e8a9e546bab51eb8
Ashok Gelal <ashok@metageek.net>
2012-12-29 20:48:48 -0700

    Create project

---
+++ src/com/ashokgelal/tagsnap/DefaultActivity.java
@@ -0,0 +1,15 @@
+package com.ashokgelal.tagsnap;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DefaultActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
